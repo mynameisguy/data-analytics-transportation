7a445f15baf67ab2d9c9744adb49a52de94111ec
cfs workload <cfsworkload@bg.vnet.ibm.com>
2015-11-11 17:08:54 +0000

    Change the kafka topic names to more generic names

--- secor/kafka/scripts/kafka-configurations.sh
+++ secor/kafka/scripts/kafka-configurations.sh
@@ -11,9 +11,9 @@ echo -e "$TEST_IP    $TEST_HOST" >>/etc/hosts
 #create kafka
 KAFKA_HOST=localhost
 
-SECORTOPIC=TrafficFlowMadridPM
-THRESHOLDTOPIC=newThresholds
+SOURCETOPIC=DataServices
+OUTPUTTOPIC=newOutput
 
-/opt/kafka_2.11-0.8.2.1/bin/kafka-topics.sh --create --zookeeper ${KAFKA_HOST}:2181 --replication-factor 1 --partition 1 --topic $SECORTOPIC
+/opt/kafka_2.11-0.8.2.1/bin/kafka-topics.sh --create --zookeeper ${KAFKA_HOST}:2181 --replication-factor 1 --partition 1 --topic $SOURCETOPIC
 
-/opt/kafka_2.11-0.8.2.1/bin/kafka-topics.sh --create --zookeeper ${KAFKA_HOST}:2181 --replication-factor 1 --partition 1 --topic $THRESHOLDTOPIC
+/opt/kafka_2.11-0.8.2.1/bin/kafka-topics.sh --create --zookeeper ${KAFKA_HOST}:2181 --replication-factor 1 --partition 1 --topic $OUTPUTTOPIC
