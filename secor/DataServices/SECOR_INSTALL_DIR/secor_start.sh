0adfe53ca2a11527b21e6554402e37aa8b81196f
Guy Hadash <guyh@il.ibm.com>
2016-09-11 14:54:51 +0300

    replacing secor jar with git clone and patch

--- secor/DataServices/SECOR_INSTALL_DIR/secor_start.sh
+++ secor/DataServices/SECOR_INSTALL_DIR/secor_start.sh
@@ -2,9 +2,17 @@
 
 export PATH=/opt/ibm/java-x86_64-71/jre/bin/:$PATH
 
-echo "downloading dependencies"
+echo "getting secor and patching it"
+git clone https://github.com/pinterest/secor.git
+cp patch.diff ./secor
+cd secor
+git checkout bf0b6abef5c786345482da7579569e00081c6126
+git apply --whitespace=nowarn --ignore-whitespace patch.diff
 mvn package
-
+cd ..
+cp secor/target/lib/* lib/
+cp secor/extraRes/messagehub.login-1.0.0.jar lib/
+cp secor/target/secor-0.1-SNAPSHOT.jar secor-0.1-SNAPSHOT.jar
 
 pkill -f 'org.apache.zookeeper.server.quorum.QuorumPeerMain'
 sleep 2
